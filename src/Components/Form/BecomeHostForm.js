e5e2073b5f3dc8f0a3cf1fa97aacc991653e947b
risaadodx <wrisaad@gmail.com>
2023-01-05 21:10:34 +0600

    allusers and allbookings page added

--- src/Components/Form/BecomeHostForm.js
+++ src/Components/Form/BecomeHostForm.js
@@ -28,8 +28,9 @@ const BecomeHostForm = ({ handleSubmit }) => {
             <div className="flex space-x-4 items-center">
               <label
                 htmlFor="image"
-                className="p-3 text-center rounded-md cursor-pointer text-gray-500 font-bold border  border-green-600 hover:bg-gradient-to-r hover:from-blue-500 hover:to-green-400 hover:border-white hover:text-white"
+                className="px-3 text-center rounded-md cursor-pointer text-gray-500  border  border-green-600 hover:bg-green-500 hover:text-white"
               >
+                Select
                 <input
                   type="file"
                   name="image"
@@ -51,7 +52,7 @@ const BecomeHostForm = ({ handleSubmit }) => {
 
             <button
               type="submit"
-              className="block w-full p-3 text-center font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-gradient-to-r from-emerald-500 to-lime-500 hover:bg-gray-200 hover:text-gray-700 focus:shadow-outline focus:outline-none"
+              className="block w-full p-3 text-center font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-green-500  hover:bg-gray-200 hover:text-gray-700 focus:shadow-outline focus:outline-none"
             >
               Submit Request
             </button>
--- src/api/imageUpload.js
+++ src/api/imageUpload.js
@@ -2,7 +2,8 @@ export const getImageUrl = async (image) => {
   const formData = new FormData();
   formData.append("image", image);
 
-  const url = `https://api.imgbb.com/1/upload?key=${process.env.REACT_APP_IMGBB_KEY}`;
+  const url =
+    "https://api.imgbb.com/1/upload?key=0ece13906c3391198b10c01424636fca";
 
   const response = await fetch(url, {
     method: "POST",
