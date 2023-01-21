f20fc62ccdebee1131afbc9f750c09c040ff2831
risaadodx <wrisaad@gmail.com>
2023-01-21 13:04:52 +0600

    mybookings page/bookings api updated,tablerow components added

--- src/api/bookings.js
+++ src/api/bookings.js
@@ -1,40 +1,42 @@
-// Save a booking
-export const saveBooking = async (bookingData) => {
-  const url = "http://localhost:5000/bookings";
+// Get all bookings for a user by email
+export const getBookings = async (email) => {
+  const response = await fetch(
+    `http://localhost:5000/bookings?email=${email}`,
+    {
+      method: "GET",
+      headers: {
+        "content-type": "application/json",
+        authorization: `Bearer ${localStorage.getItem("aircnc-token")}`,
+      },
+    }
+  );
+  const bookings = await response.json();
+  return bookings;
+};
 
-  const response = await fetch(url, {
-    method: "POST",
+// Get All bookings for admin
+export const getAllBookings = async () => {
+  const response = await fetch("http://localhost:5000/bookings", {
+    method: "GET",
     headers: {
       "content-type": "application/json",
+      authorization: `Bearer ${localStorage.getItem("aircnc-token")}`,
     },
-    body: JSON.stringify(bookingData),
+  });
+  const bookings = await response.json();
+  return bookings;
+};
+
+// Delete a booking
+export const deleteBooking = async (id) => {
+  const response = await fetch(`http://localhost:5000/booking/${id}`, {
+    method: "DELETE",
+    headers: {
+      "content-type": "application/json",
+      authorization: `Bearer ${localStorage.getItem("aircnc-token")}`,
+    },
   });
 
   const data = await response.json();
-
   return data;
 };
-
-// Get All Bookings for user
-export const getAllBookingsByEmail = async (email) => {
-  const url = `http://localhost:5000/bookings?email=${email}`;
-
-  const response = await fetch(url);
-
-  const data = await response.json();
-
-  return data;
-};
-
-// Get All Bookings for Admin
-export const getAllBookings = async () => {
-  const url = "http://localhost:5000/bookings";
-
-  const response = await fetch(url);
-
-  const data = await response.json();
-
-  return data;
-};
-
-// Cancel A booking
